5017dbd4cfbf7b8c30cbd3f85b57909bbcd3aef7
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2022-09-08 11:15:06 +0200

    Fix: Wrong file string conversion
    When a File is used in the FluentBundleBuilder it would just use the path as the file itself.
    
    This is now changed so that the file itself is getting read.

--- src/main/java/net/quickwrite/fluent4j/builder/FluentBundleBuilder.java
+++ src/main/java/net/quickwrite/fluent4j/builder/FluentBundleBuilder.java
@@ -6,6 +6,9 @@ import net.quickwrite.fluent4j.functions.AbstractFunction;
 import net.quickwrite.fluent4j.parser.FluentParser;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * The builder class for the {@link FluentBundle} class.
@@ -42,9 +45,51 @@ public class FluentBundleBuilder extends AbstractBuilder<FluentBundle> {
      *
      * @param locale The language that the bundle should have
      * @param file   The file itself that should be parsed
+     * @throws IOException If an I/O error occurs reading from the
+     *                     file or a malformed or unmappable byte
+     *                     sequence is read
      */
-    public FluentBundleBuilder(final ULocale locale, final File file) {
-        this(locale, file.toString());
+    public FluentBundleBuilder(final ULocale locale, final File file) throws IOException {
+        this(locale, file.toPath());
+    }
+
+    /**
+     * <p>
+     * Creates a new FluentBundleBuilder with the necessary parameters
+     * for a {@link FluentBundle}.
+     * </p>
+     * <p>
+     * A bundle itself is a collection of different messages that can
+     * be accessed and built with the necessary parameters.
+     * </p>
+     * <p>
+     * If there are duplicate messages like
+     * <pre>
+     *     # This is the first message
+     *     test = Hello World
+     *
+     *     # This is the second message
+     *     test = This is the text that overwrites
+     * </pre>
+     * then the last message will override the previous message so that
+     * when {@code test} is getting called the result will be
+     * {@code This is the text that overwrites} and not {@code Hello World}.
+     * </p>
+     * <p>
+     * All of the error messages that are getting thrown at the parsing
+     * step are getting saved in the FluentBundle and can be accessed
+     * by using the methods {@link FluentBundle#hasExceptions()} and
+     * {@link FluentBundle#getExceptionList()}.
+     * </p>
+     *
+     * @param locale The language that the bundle should have
+     * @param path   The path to the file that should be parsed
+     * @throws IOException If an I/O error occurs reading from the
+     *                     file or a malformed or unmappable byte
+     *                     sequence is read
+     */
+    public FluentBundleBuilder(final ULocale locale, final Path path) throws IOException {
+        this(locale, Files.readString(path));
     }
 
     /**
@@ -68,7 +113,7 @@ public class FluentBundleBuilder extends AbstractBuilder<FluentBundle> {
      * {@code This is the text that overwrites} and not {@code Hello World}.
      *
      * <p>
-     * All of the error messages that are getting thrown at the parsing
+     * All the error messages that are getting thrown at the parsing
      * step are getting saved in the FluentBundle and can be accessed
      * by using the methods {@link FluentBundle#hasExceptions()} and
      * {@link FluentBundle#getExceptionList()}.
