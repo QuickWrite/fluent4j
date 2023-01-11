82547ddf3420f660b3486cf1e62c3efb50c1ffcc
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-01-11 11:25:28 +0100

    Remove the entire project
    Why: Just redoing it.

--- src/main/java/net/quickwrite/fluent4j/exception/UnknownElementException.java
+++
@@ -1,7 +0,0 @@
-package net.quickwrite.fluent4j.exception;
-
-public class UnknownElementException extends RuntimeException {
-    public UnknownElementException(final String reason) {
-        super(reason);
-    }
-}
