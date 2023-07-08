60763ee3bc3a2c256f74602da0a3365cad1191c9
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-07-08 18:45:11 +0200

    Add some comments to the base interfaces

--- api/src/main/java/net/quickwrite/fluent4j/ast/FluentResolvable.java
+++ api/src/main/java/net/quickwrite/fluent4j/ast/FluentResolvable.java
@@ -3,6 +3,15 @@ package net.quickwrite.fluent4j.ast;
 import net.quickwrite.fluent4j.container.FluentScope;
 import net.quickwrite.fluent4j.result.ResultBuilder;
 
+/**
+ * The base element for the construction of the message.
+ *
+ * @param <B> The type of ResultBuilder associated with the resolvable entity.
+ */
 public interface FluentResolvable<B extends ResultBuilder> {
+    /**
+     * @param scope The scope which contains the necessary information for this element.
+     * @param builder The ResultBuilder used for resolving the entity.
+     */
     void resolve(final FluentScope<B> scope, final B builder);
 }
