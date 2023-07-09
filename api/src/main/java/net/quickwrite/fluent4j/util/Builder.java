a5f40cbdb43ff1a7540256e0c81c814c681bc17e
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-07-09 18:09:33 +0200

    Add Docs to builder

--- api/src/main/java/net/quickwrite/fluent4j/util/Builder.java
+++ api/src/main/java/net/quickwrite/fluent4j/util/Builder.java
@@ -2,15 +2,21 @@ package net.quickwrite.fluent4j.util;
 
 import java.util.function.Consumer;
 
+/**
+ * The Builder interface represents a generic builder.
+ *
+ * @param <R> the type to be built
+ */
 public interface Builder<R> {
+
     /**
-     * Configures {@code builder} using {@code consumer} and then builds.
+     * Configures the builder using the provided consumer and then builds the object.
      *
-     * @param builder the builder
-     * @param consumer the builder consume
-     * @param <R> the type to be built
-     * @param <B> the builder type
-     * @return the built thing
+     * @param builder  the builder instance
+     * @param consumer the consumer to configure the builder
+     * @param <R>      the type to be built
+     * @param <B>      the builder type
+     * @return the built object
      */
     static <R, B extends Builder<R>> R configureAndBuild(final B builder, final Consumer<? super B> consumer) {
         if (consumer != null) {
@@ -19,5 +25,10 @@ public interface Builder<R> {
         return builder.build();
     }
 
+    /**
+     * Builds the object.
+     *
+     * @return the built object
+     */
     R build();
 }
