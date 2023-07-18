8b0820b6e133c0f3136c205ea9aa9365a4d50089
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-07-18 20:22:31 +0200

    Docs: Content interfaces

--- api/src/main/java/net/quickwrite/fluent4j/parser/pattern/FluentContentParser.java
+++ api/src/main/java/net/quickwrite/fluent4j/parser/pattern/FluentContentParser.java
@@ -7,7 +7,22 @@ import net.quickwrite.fluent4j.result.ResultBuilder;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * A list of parsers for parsing the contents of a message
+ *
+ * @param <B> The Builder that is being used to parse this resource
+ */
 public interface FluentContentParser<B extends ResultBuilder> {
+    /**
+     * Parses the content of an element into a list of {@link FluentPattern}
+     * until it reaches the end of the input determined either by the
+     * {@code iterator} or by the {@code endChecker}.
+     *
+     * @param iterator The iterator that the parser should run over
+     * @param endChecker The function that determines if the current
+     *                   state of the iterator is defined as the end
+     * @return A list of the parsed {@link FluentPattern}
+     */
     List<FluentPattern<B>> parse(final ContentIterator iterator, final Function<ContentIterator, Boolean> endChecker);
 
     interface Builder<B extends ResultBuilder> extends net.quickwrite.fluent4j.util.Builder<FluentContentParser<B>> {
--- api/src/main/java/net/quickwrite/fluent4j/parser/pattern/FluentPatternParser.java
+++ api/src/main/java/net/quickwrite/fluent4j/parser/pattern/FluentPatternParser.java
@@ -5,8 +5,37 @@ import net.quickwrite.fluent4j.iterator.ContentIterator;
 import net.quickwrite.fluent4j.parser.result.ParseResult;
 import net.quickwrite.fluent4j.result.ResultBuilder;
 
+/**
+ * The basic pattern parser interface
+ *
+ * <p>
+ *     A pattern itself is a component of the content
+ *     of a message. E. g.:
+ *     <pre>
+ *         message = pattern { $pattern2 }
+ *     </pre>
+ * </p>
+ *
+ * @param <T> The resulting pattern
+ * @param <B> The Builder that is being used to parse this resource
+ */
 public interface FluentPatternParser<T extends FluentPattern<B>, B extends ResultBuilder> {
+    /**
+     * Returns the starting character of the component
+     *
+     * @return The starting char
+     */
     int getStartingChar();
 
+    /**
+     * Returns the parsed version of the next iterator
+     * sequence for the specific pattern.
+     *
+     * @param iterator The iterator that should be parsed
+     * @param contentParser The parser that can be used to
+     *                      recursively parse the content like
+     *                      the message content
+     * @return The result of the parsing operation
+     */
     ParseResult<T> parse(final ContentIterator iterator, final FluentContentParser<B> contentParser);
 }
