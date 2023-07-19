7c1853b2e8b26f9c2058cee6af5678cb14e901ec
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-07-19 21:15:56 +0200

    Docs: Add docs for PlaceableParsers

--- api/src/main/java/net/quickwrite/fluent4j/parser/pattern/placeable/PlaceableExpressionParser.java
+++ api/src/main/java/net/quickwrite/fluent4j/parser/pattern/placeable/PlaceableExpressionParser.java
@@ -6,6 +6,19 @@ import net.quickwrite.fluent4j.result.ResultBuilder;
 
 import java.util.Optional;
 
+/**
+ * The parser for the actual different Placeables.
+ *
+ * <p>
+ *     This parser should only parse from the actual start of
+ *     the element inside of the Placeable and not the start of
+ *     the placeable itself.
+ *     <br />
+ *     For example with the placeable {@code { 42 } } it would
+ *     parse from the {@code 4} to the {@code }} and close it.
+ * </p>
+ * @param <B> The Builder that is being used to parse this resource
+ */
 public interface PlaceableExpressionParser<B extends ResultBuilder> {
     Optional<FluentPlaceable<B>> parse(final ContentIterator iterator, final PlaceableParser<B> placeableParser);
 
