21d9f7e8329ab0226b4accc6eab467bea656ec67
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-08-15 23:05:33 +0200

    Add a file content iterator

--- builder/src/test/java/net/quickwrite/fluent4j/test/util/FluentUtils.java
+++ builder/src/test/java/net/quickwrite/fluent4j/test/util/FluentUtils.java
@@ -1,29 +1,29 @@
 package net.quickwrite.fluent4j.test.util;
 
 import net.quickwrite.fluent4j.container.FluentResource;
-import net.quickwrite.fluent4j.iterator.StringIteratorFactory;
+import net.quickwrite.fluent4j.iterator.FluentIteratorFactory;
 import net.quickwrite.fluent4j.parser.ResourceParser;
 import net.quickwrite.fluent4j.parser.ResourceParserBuilder;
 import net.quickwrite.fluent4j.result.ResultBuilder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class FluentUtils {
     public static <B extends ResultBuilder> FluentResource<B> getResourceFromString(final String string) {
         final ResourceParser<B> resourceParser = ResourceParserBuilder.defaultParser();
 
-        return resourceParser.parse(StringIteratorFactory.from(string));
+        return resourceParser.parse(FluentIteratorFactory.fromString(string));
     }
 
     public static <B extends ResultBuilder> FluentResource<B> getResourceFromResource(final String string) {
         try {
-            return getResourceFromString(getResourceFileAsString(string));
-        } catch (final IOException exception) {
-            throw new RuntimeException();
+            final ResourceParser<B> resourceParser = ResourceParserBuilder.defaultParser();
+
+            return resourceParser.parse(FluentIteratorFactory.fromFile(getResourceFileAsFile(string)));
+        } catch (final IOException | URISyntaxException exception) {
+            throw new RuntimeException(exception);
         }
     }
 
@@ -32,16 +32,12 @@ public class FluentUtils {
      *
      * @param fileName path to the resource file
      * @return the file's contents
-     * @throws IOException if read fails for any reason
+     * @throws URISyntaxException If the action fails for some reason
      */
-    private static String getResourceFileAsString(String fileName) throws IOException {
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-        try (InputStream is = classLoader.getResourceAsStream(fileName)) {
-            if (is == null) return null;
-            try (InputStreamReader isr = new InputStreamReader(is);
-                 BufferedReader reader = new BufferedReader(isr)) {
-                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
-            }
-        }
+    private static File getResourceFileAsFile(String fileName) throws URISyntaxException {
+        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        final URL url = classLoader.getResource(fileName);
+        assert url != null;
+        return new File(url.toURI());
     }
 }
--- impl/src/main/java/net/quickwrite/fluent4j/impl/iterator/StringContentIterator.java
+++ impl/src/main/java/net/quickwrite/fluent4j/impl/iterator/StringContentIterator.java
@@ -1,83 +1,12 @@
 package net.quickwrite.fluent4j.impl.iterator;
 
-import net.quickwrite.fluent4j.iterator.ContentIterator;
-
-public final class StringContentIterator implements ContentIterator {
-    private final String[] lines;
-    private int line;
-    private int characterIndex;
-
-    public StringContentIterator(final String input) {
-        this.lines = input.lines().toArray(String[]::new);
-        this.line = 0;
-        this.characterIndex = 0;
+public final class StringContentIterator extends LineContentIterator {
+    public StringContentIterator(final String content) {
+        super(content.lines().toArray(String[]::new));
     }
 
     @Override
     public String inputName() {
         return "String";
-    }
-
-    @Override
-    public int character() {
-        if (line() == null || line().length() < this.characterIndex) {
-            return 0;
-        }
-
-        if (line().length() == this.characterIndex) {
-            return '\n';
-        }
-
-        return line().codePointAt(characterIndex);
-    }
-
-    @Override
-    public int nextChar() {
-        if (line() == null) {
-            return 0;
-        }
-
-        if (line().length() < this.characterIndex + 1) {
-            this.characterIndex = 0;
-            if (nextLine() == null) {
-                return 0;
-            }
-
-            return character();
-        }
-
-        if(line().codePointAt(characterIndex) > Character.MAX_VALUE) {
-            this.characterIndex++;
-        }
-        this.characterIndex++;
-
-        return character();
-    }
-
-    @Override
-    public String line() {
-        if (lines.length <= line) {
-            return null;
-        }
-
-        return lines[line];
-    }
-
-    @Override
-    public String nextLine() {
-        line++;
-        characterIndex = 0;
-        return line();
-    }
-
-    @Override
-    public int[] position() {
-        return new int[]{line, characterIndex};
-    }
-
-    @Override
-    public void setPosition(final int[] position) {
-        this.line = position[0];
-        this.characterIndex = position[1];
     }
 }
