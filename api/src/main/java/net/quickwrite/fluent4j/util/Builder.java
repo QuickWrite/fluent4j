92873f47347d607c9789990c3f1e31b212d96035
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2023-09-17 19:16:50 +0200

    Remove `configureAndBuild`-method
    As the method wasn't being used and can easily be replicated with the existing API it can easily be removed.

--- api/src/main/java/net/quickwrite/fluent4j/util/Builder.java
+++ api/src/main/java/net/quickwrite/fluent4j/util/Builder.java
@@ -1,30 +1,11 @@
 package net.quickwrite.fluent4j.util;
 
-import java.util.function.Consumer;
-
 /**
  * The Builder interface represents a generic builder.
  *
  * @param <R> the type to be built
  */
 public interface Builder<R> {
-
-    /**
-     * Configures the builder using the provided consumer and then builds the object.
-     *
-     * @param builder  the builder instance
-     * @param consumer the consumer to configure the builder
-     * @param <R>      the type to be built
-     * @param <B>      the builder type
-     * @return the built object
-     */
-    static <R, B extends Builder<R>> R configureAndBuild(final B builder, final Consumer<? super B> consumer) {
-        if (consumer != null) {
-            consumer.accept(builder);
-        }
-        return builder.build();
-    }
-
     /**
      * Builds the object.
      *
